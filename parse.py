c71ae180c591985dd06706d3d08e9ddd47b91886
Logan Graves <me@logangraves.com>
2024-08-25 10:56:12 -0700

    add syntax highlighting using pygment -- black and white for now
    Signed-off-by: Logan Graves <me@logangraves.com>

--- parse.py
+++ parse.py
@@ -1,6 +1,14 @@
 from markdown import markdown
 import os 
+import pygments
+import re
 
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import guess_lexer
+
+import html
 
 parent_directory = "markdown/"
 list_of_file_names = os.listdir(parent_directory)
@@ -63,6 +71,7 @@ for file_name in list_of_file_names:
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>new life</title>
             <link rel="stylesheet" href="site.css">
+            <link rel="stylesheet" href="theme.css">
         </head>
         <body>
             <div id="wrapper">
@@ -70,11 +79,30 @@ for file_name in list_of_file_names:
             </div>
             <script type="text/javascript" src="mathjax.js"></script>
             <script type="text/javascript" id="MathJax-script" src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"></script>
-	    <script src="microlight/microlight.js"></script>
 
         </body>
         </html>
         """
-	
+
+        content_formatted = completed_template
+
+        for code_section in re.findall('\<pre\>\<code\>[\s\S]*?\<\/code\>\<\/pre\>', content_formatted):
+	        # print(code_section)
+                new_code_section = code_section.replace('<pre><code>', '')
+                new_code_section = new_code_section.replace('</code></pre>', '')
+
+                new_code_section = html.unescape(new_code_section)
+
+                lexer = get_lexer_by_name("python", stripall=True)
+                formatter = HtmlFormatter(linenos=False, cssclass="bw", style='bw')
+
+                new_code_section_highlight = highlight(new_code_section, lexer, formatter)
+                content_formatted = content_formatted.replace(code_section, new_code_section_highlight)
+        # print(content_formatted)
         # completed_template = add_code_class(completed_template)
-        file.write(completed_template)
+        file.write(content_formatted)
+
+css = HtmlFormatter(style="bw").get_style_defs('.bw')
+
+with open("theme.css", "w") as file:
+	file.write(css)
